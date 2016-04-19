4466b8efbcb06c3370967eb643f668ec29e9dd8d
John <xpixelragex@gmail.com>
2016-04-19 14:33:43 -0500

    removed need to have egg item

--- src/com/ninjaguild/dragoneggdrop/DragonDeathRunnable.java
+++ src/com/ninjaguild/dragoneggdrop/DragonDeathRunnable.java
@@ -4,8 +4,6 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
-import org.bukkit.entity.Item;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class DragonDeathRunnable implements Runnable {
@@ -20,15 +18,15 @@ public class DragonDeathRunnable implements Runnable {
 	private double oY = 0D;
 	private double oZ = 0D;
 	private Particle particleType = null;
-	
+
 	private World world = null;
-	
+
 	private boolean placeEgg = false;
 
 	public DragonDeathRunnable(DragonEggDrop plugin, World world, boolean prevKilled) {
 		this.plugin = plugin;
 		this.world = world;
-		
+
 		this.placeEgg = prevKilled;
 
 		particleAmount = plugin.getConfig().getInt("particle-amount", 4);
@@ -40,64 +38,57 @@ public class DragonDeathRunnable implements Runnable {
 		oZ = plugin.getConfig().getDouble("particle-offset-z", 0.25D);
 		particleType = Particle.valueOf(plugin.getConfig().getString("particle-type", "FLAME").toUpperCase());
 	}
-	
+
 	@Override
 	public void run() {
 		double startY = plugin.getConfig().getDouble("egg-start-y", 180D);
-		
-		Item egg = world.dropItem(new Location(world, 0.5D, startY, 0.5D), new ItemStack(Material.DRAGON_EGG));
-		egg.setPickupDelay(Integer.MAX_VALUE);
-		
+
 		new BukkitRunnable()
 		{
 			double currentY = startY;
-			
+			Location pLoc = new Location(world, 0.5D, currentY, 0.5D, 0f, 90f);
+
 			@Override
 			public void run() {
-				//don't know why I keep having to TP it back to correct x,z :/
-				egg.teleport(new Location(egg.getWorld(), 0.5D, currentY, 0.5D));
 				currentY -= 1D;
-				
+				pLoc.setY(currentY);
+
 				for (double d = 0; d < particleLength; d+=0.1D) {
-					Location particleLoc = egg.getLocation().clone().add(egg.getVelocity().normalize().multiply(d * -1));
-					egg.getWorld().spawnParticle(particleType, particleLoc, particleAmount, oX, oY, oZ, particleExtra, null);
+					world.spawnParticle(particleType, pLoc.clone().add(pLoc.getDirection().normalize().multiply(d * -1)),
+							particleAmount, oX, oY, oZ, particleExtra, null);
 				}
-				
-				if (egg == null || egg.isOnGround() || egg.isDead()) {
+
+				if (currentY <= 63) {
 					cancel();
 
-					if (egg.isOnGround()) {
-						Location eggLoc = egg.getLocation();
-						double eX = eggLoc.getX();
-						double eY = eggLoc.getY();
-						double eZ = eggLoc.getZ();
-						egg.remove();
-						
-						new BukkitRunnable()
-						{
-							@Override
-							public void run() {
-								eggLoc.getWorld().createExplosion(eX, eY, eZ, 0f, false, false);
+					double eX = pLoc.getX();
+					double eY = pLoc.getY();
+					double eZ = pLoc.getZ();
 
-								int lightningAmount = plugin.getConfig().getInt("lightning-amount", 4);
-								eggLoc.getWorld().strikeLightningEffect(eggLoc);
-								if (lightningAmount > 1) {
-									for (int i = 0; i < (lightningAmount - 1); i++) {
-										eggLoc.getWorld().spigot().strikeLightningEffect(eggLoc, true);
-									}
-								}
+					new BukkitRunnable()
+					{
+						@Override
+						public void run() {
+							world.createExplosion(eX, eY, eZ, 0f, false, false);
 
-								if (placeEgg) {
-								    eggLoc.getWorld().getBlockAt(egg.getLocation()).setType(Material.DRAGON_EGG);
+							int lightningAmount = plugin.getConfig().getInt("lightning-amount", 4);
+							world.strikeLightningEffect(pLoc);
+							if (lightningAmount > 1) {
+								for (int i = 0; i < (lightningAmount - 1); i++) {
+									world.spigot().strikeLightningEffect(pLoc, true);
 								}
 							}
 
-						}.runTask(plugin);
-					}
+							if (placeEgg) {
+								world.getBlockAt(new Location(world, 0.5D, 63D, 0.5D)).setType(Material.DRAGON_EGG);
+							}
+						}
+
+					}.runTask(plugin);
 				}
 			}
 
 		}.runTaskTimerAsynchronously(plugin, 0L, particleInterval);
 	}
-	
+
 }
