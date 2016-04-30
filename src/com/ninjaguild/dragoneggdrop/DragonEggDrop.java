d8f04596ef280ef389477a8c359d21d9e93cc57c
John <xpixelragex@gmail.com>
2016-04-29 23:12:23 -0500

    progress for 1.1.0

--- src/com/ninjaguild/dragoneggdrop/DragonEggDrop.java
+++ src/com/ninjaguild/dragoneggdrop/DragonEggDrop.java
@@ -1,5 +1,8 @@
 package com.ninjaguild.dragoneggdrop;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 
 import org.bukkit.Particle;
@@ -9,19 +12,26 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import net.minecraft.server.v1_9_R1.BossBattleServer;
+import net.minecraft.server.v1_9_R1.ChatMessage;
+import net.minecraft.server.v1_9_R1.EnderDragonBattle;
 import net.minecraft.server.v1_9_R1.EntityEnderDragon;
+import net.minecraft.server.v1_9_R1.PacketPlayOutBoss;
 
 import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftEnderDragon;
 
 public class DragonEggDrop extends JavaPlugin implements Listener {
 
-	PluginDescriptionFile pdf = null;
+	private PluginDescriptionFile pdf = null;
+	private List<String> dragonNames = null;
+	private Random rand = null;
 	
 	public void onEnable() {
 		saveDefaultConfig();
@@ -45,12 +55,34 @@ public class DragonEggDrop extends JavaPlugin implements Listener {
 		
 		getServer().getPluginManager().registerEvents(this, this);
 		getCommand("dragoneggdrop").setExecutor(this);
+		
+		rand = new Random();
+		dragonNames = getConfig().getStringList("dragon-names");
 	}
 	
 	public void onDisable() {
 		//
 	}
 
+	@EventHandler
+	public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getEntityType() == EntityType.ENDER_DRAGON) {
+            if (!dragonNames.isEmpty()) {
+            	String name = dragonNames.get(rand.nextInt(dragonNames.size()));
+				try {
+					Field f = EnderDragonBattle.class.getDeclaredField("c");
+					f.setAccessible(true);
+					BossBattleServer battleServer = (BossBattleServer)f.get(((CraftEnderDragon)e.getEntity()).getHandle().cU());
+					battleServer.title = new ChatMessage(name, new Object[0]);
+					battleServer.sendUpdate(PacketPlayOutBoss.Action.UPDATE_NAME);
+					f.setAccessible(false);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+            }
+        }
+	}
+	
 	@EventHandler
 	private void onDragonDeath(EntityDeathEvent e) {
 		if (e.getEntityType() == EntityType.ENDER_DRAGON) {
