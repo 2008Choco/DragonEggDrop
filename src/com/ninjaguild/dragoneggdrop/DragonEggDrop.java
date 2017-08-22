454e9eafda6a5d5231150d62ec0a0fb58f1efd8d
Parker Hawke <hawkeboyz2@hotmail.com>
2017-08-22 17:01:59 -0400

    Attempt to fix issues regarding /reload with temp JSON data

--- src/com/ninjaguild/dragoneggdrop/DragonEggDrop.java
+++ src/com/ninjaguild/dragoneggdrop/DragonEggDrop.java
@@ -20,16 +20,27 @@
 package com.ninjaguild.dragoneggdrop;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.Map.Entry;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.logging.Level;
 
+import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.stream.JsonWriter;
 import com.ninjaguild.dragoneggdrop.commands.DragonEggDropCmd;
 import com.ninjaguild.dragoneggdrop.commands.DragonTemplateCmd;
 import com.ninjaguild.dragoneggdrop.dragon.DragonTemplate;
@@ -38,6 +49,7 @@ import com.ninjaguild.dragoneggdrop.events.LootListeners;
 import com.ninjaguild.dragoneggdrop.events.PortalClickListener;
 import com.ninjaguild.dragoneggdrop.events.RespawnListeners;
 import com.ninjaguild.dragoneggdrop.management.DEDManager;
+import com.ninjaguild.dragoneggdrop.management.EndWorldWrapper;
 import com.ninjaguild.dragoneggdrop.utils.ConfigUtil;
 import com.ninjaguild.dragoneggdrop.versions.NMSAbstract;
 import com.ninjaguild.dragoneggdrop.versions.v1_10.NMSAbstract1_10_R1;
@@ -49,7 +61,9 @@ import com.ninjaguild.dragoneggdrop.versions.v1_9.NMSAbstract1_9_R2;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -65,6 +79,7 @@ import org.bukkit.scheduler.BukkitTask;
 public class DragonEggDrop extends JavaPlugin {
 	
 	private static final String CHAT_PREFIX = ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "DED" + ChatColor.DARK_GRAY + "] " + ChatColor.GRAY;
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 	
 	/* TODO for version (or 1.4.0)
 	 * - Aesthetic oriented update
@@ -82,6 +97,8 @@ public class DragonEggDrop extends JavaPlugin {
 	private NMSAbstract nmsAbstract;
 	
 	private BukkitTask updateTask;
+	
+	private File tempDataFile;
 
 	@Override
 	public void onEnable() {
@@ -113,6 +130,13 @@ public class DragonEggDrop extends JavaPlugin {
 		}
 		
 		this.dedManager = new DEDManager(this);
+		
+		// Load temp data (reload support)
+		this.tempDataFile = new File(getDataFolder(), "tempData.json");
+		if (tempDataFile.exists()) {
+			this.readTempData(tempDataFile);
+			this.tempDataFile.delete();
+		}
 		
 		// Register events
 		Bukkit.getPluginManager().registerEvents(new DragonLifeListeners(this), this);
@@ -147,6 +171,15 @@ public class DragonEggDrop extends JavaPlugin {
 	public void onDisable() {
 		if (this.updateTask != null)
 			this.updateTask.cancel();
+		
+		if (!tempDataFile.exists()) {
+			try {
+				this.tempDataFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		this.writeTempData(tempDataFile);
 		
 		this.dedManager.clearTemplates();
 		
@@ -216,6 +249,58 @@ public class DragonEggDrop extends JavaPlugin {
 				this.saveResource(name, false);
 			}
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeTempData(File file) {
+		JsonObject root = new JsonObject();
+		
+		for (EndWorldWrapper world : dedManager.getWorldWrappers().values()) {
+			if (!world.isRespawnInProgress() && world.getActiveBattle() == null) return;
+			
+			JsonObject jsonWorld = new JsonObject();
+			
+			if (world.isRespawnInProgress()) jsonWorld.addProperty("respawnTime", world.getTimeUntilRespawn());
+			if (world.getActiveBattle() != null) jsonWorld.addProperty("activeTemplate", world.getActiveBattle().getFile().getName());
+			
+			root.add(world.getWorld().getName(), jsonWorld);
+		}
+		
+		try (PrintWriter writer = new PrintWriter(tempDataFile)) {
+			GSON.toJson(root, new JsonWriter(writer));
+		} catch (JsonIOException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void readTempData(File file) {
+		try (FileReader reader = new FileReader(tempDataFile)) {
+			JsonObject root = GSON.fromJson(reader, JsonObject.class);
+			
+			for (Entry<String, JsonElement> entry : root.entrySet()) {
+				World world = Bukkit.getWorld(entry.getKey());
+				if (world == null) return;
+				
+				EndWorldWrapper wrapper = dedManager.getWorldWrapper(world);
+				JsonObject element = entry.getValue().getAsJsonObject();
+				
+				if (element.has("respawnTime")) {
+					if (wrapper.isRespawnInProgress()) wrapper.stopRespawn();
+					
+					wrapper.startRespawn(element.get("respawnTime").getAsInt(), getConfig().getBoolean("announce-respawn", true));
+				}
+				
+				Collection<EnderDragon> dragons = world.getEntitiesByClass(EnderDragon.class);
+				if (element.has("activeTemplate") && !dragons.isEmpty()) {
+					DragonTemplate template = dedManager.getTemplate(element.get("activeTemplate").getAsString());
+					if (template == null) return;
+					
+					wrapper.setActiveBattle(template);
+					template.applyToBattle(nmsAbstract, Iterables.get(dragons, 0), nmsAbstract.getEnderDragonBattleFromWorld(world));
+				}
+			}
+		} catch (IOException | JsonParseException e) {
 			e.printStackTrace();
 		}
 	}
