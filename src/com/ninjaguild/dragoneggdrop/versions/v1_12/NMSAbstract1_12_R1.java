23af6b5224c1d8c72be24e2965df8fe19a070b04
Parker Hawke <hawkeboyz2@hotmail.com>
2017-08-28 23:06:07 -0400

    Fixed infinite lightning due to new class heirarchy and method modifier

--- src/com/ninjaguild/dragoneggdrop/versions/v1_12/NMSAbstract1_12_R1.java
+++ src/com/ninjaguild/dragoneggdrop/versions/v1_12/NMSAbstract1_12_R1.java
@@ -19,27 +19,31 @@
 
 package com.ninjaguild.dragoneggdrop.versions.v1_12;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import com.ninjaguild.dragoneggdrop.versions.DragonBattle;
 import com.ninjaguild.dragoneggdrop.versions.NMSAbstract;
+
+import org.bukkit.World;
+import org.bukkit.block.Chest;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_12_R1.block.CraftBlockEntityState;
+import org.bukkit.craftbukkit.v1_12_R1.block.CraftChest;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEnderDragon;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Player;
 
 import net.minecraft.server.v1_12_R1.ChatMessageType;
 import net.minecraft.server.v1_12_R1.EnderDragonBattle;
 import net.minecraft.server.v1_12_R1.EntityEnderDragon;
 import net.minecraft.server.v1_12_R1.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
+import net.minecraft.server.v1_12_R1.TileEntityChest;
 import net.minecraft.server.v1_12_R1.WorldProvider;
 import net.minecraft.server.v1_12_R1.WorldProviderTheEnd;
-
-import org.bukkit.World;
-import org.bukkit.block.Chest;
-import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_12_R1.block.CraftChest;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEnderDragon;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
-import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.Player;
 
 /**
  * An abstract implementation of necessary net.minecraft.server and
@@ -93,7 +97,34 @@ public class NMSAbstract1_12_R1 implements NMSAbstract {
 		if (chest == null || name == null) return;
 		
 		CraftChest craftChest = (CraftChest) chest;
-		craftChest.getTileEntity().setCustomName(name);
+		
+		// CraftChest#getTileEntity() was moved up to CraftBlockEntityState#getTileEntity() and made protected
+		// TODO Remove unnecessary checks when implementing this code for 1.13
+		try {
+			boolean entityStateExists = false;
+			try {
+				Class.forName("org.bukkit.craftbukkit.block.CraftBlockEntityState");
+				entityStateExists = true;
+			} catch (ClassNotFoundException e) { /* ignore */ }
+			
+			Method methodGetTileEntity;
+			
+			if (entityStateExists) {
+				methodGetTileEntity = CraftBlockEntityState.class.getDeclaredMethod("getTileEntity");
+			}
+			else {
+				methodGetTileEntity = CraftChest.class.getMethod("getTileEntity");
+			}
+			
+			methodGetTileEntity.setAccessible(true);
+			
+			TileEntityChest chestEntity = (TileEntityChest) methodGetTileEntity.invoke(craftChest);
+			chestEntity.setCustomName(name);
+			
+			methodGetTileEntity.setAccessible(false);
+		} catch (NoSuchMethodException | IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
