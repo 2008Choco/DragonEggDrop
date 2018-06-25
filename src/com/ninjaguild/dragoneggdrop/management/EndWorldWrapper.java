db839412921892a274c9b9a5c0efc54c233c326d
Parker Hawke <hawkeboyz2@hotmail.com>
2018-06-25 11:19:34 -0400

    Support time suffixes in time-based configuration options. Closes #8

--- src/com/ninjaguild/dragoneggdrop/management/EndWorldWrapper.java
+++ src/com/ninjaguild/dragoneggdrop/management/EndWorldWrapper.java
@@ -1,6 +1,9 @@
 package com.ninjaguild.dragoneggdrop.management;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.ninjaguild.dragoneggdrop.DragonEggDrop;
 import com.ninjaguild.dragoneggdrop.dragon.DragonTemplate;
@@ -8,6 +11,7 @@ import com.ninjaguild.dragoneggdrop.management.DEDManager.RespawnType;
 import com.ninjaguild.dragoneggdrop.utils.runnables.RespawnRunnable;
 
 import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
@@ -21,6 +25,8 @@ import org.bukkit.entity.EnderDragon;
  * @author Parker Hawke - 2008Choco
  */
 public class EndWorldWrapper {
+	
+	private static final Pattern TIME_PATTERN = Pattern.compile("(\\d+)([wdhms])");
 	
 	private RespawnRunnable respawnTask;
 	
@@ -65,7 +71,7 @@ public class EndWorldWrapper {
 		if (dragonExists || respawnInProgress || respawnTask != null) return;
 		
         FileConfiguration config = plugin.getConfig();
-		int respawnDelay = (type == RespawnType.JOIN ? config.getInt("join-respawn-delay", 60) : config.getInt("death-respawn-delay", 300));
+		int respawnDelay = parseRespawnSeconds(type == RespawnType.JOIN ? config.getString("join-respawn-delay", "1m") : config.getString("death-respawn-delay", "5m"));
 		
 		this.respawnTask = new RespawnRunnable(plugin, getWorld(), respawnDelay);
 		this.respawnTask.runTaskTimer(plugin, 0, 20);
@@ -137,6 +143,41 @@ public class EndWorldWrapper {
 	 */
 	public DragonTemplate getActiveBattle() {
 		return activeBattle;
+	}
+	
+	private int parseRespawnSeconds(String value) {
+		// Handle legacy (i.e. no timestamps... for example, just "600")
+		int legacyTime = NumberUtils.toInt(value, -1);
+		if (legacyTime != -1) {
+			return legacyTime;
+		}
+		
+		int seconds = 0;
+		
+		Matcher matcher = TIME_PATTERN.matcher(value);
+		while (matcher.find()) {
+			int amount = NumberUtils.toInt(matcher.group(1));
+			
+			switch (matcher.group(2)) {
+				case "w":
+					seconds += TimeUnit.DAYS.toSeconds(amount * 7);
+					break;
+				case "d":
+					seconds += TimeUnit.DAYS.toSeconds(amount);
+					break;
+				case "h":
+					seconds += TimeUnit.HOURS.toSeconds(amount);
+					break;
+				case "m":
+					seconds += TimeUnit.MINUTES.toSeconds(amount);
+					break;
+				case "s":
+					seconds += amount;
+					break;
+			}
+		}
+		
+		return seconds;
 	}
 	
 }
