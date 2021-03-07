9108547f08d2895893cd35763c20df6d41c4ea07
Parker Hawke <hawkeboyz2@hotmail.com>
2021-03-07 16:13:51 -0500

    Fix bar style and colour not being parsed properly (backwards... oops)

--- src/main/java/wtf/choco/dragoneggdrop/dragon/DragonTemplate.java
+++ src/main/java/wtf/choco/dragoneggdrop/dragon/DragonTemplate.java
@@ -308,14 +308,14 @@ public class DragonTemplate implements Registerable {
             templateBuilder.name(dragonName);
         }
 
-        String barStyleString = templateFile.getString(DEDConstants.TEMPLATE_BAR_COLOR, "SOLID");
-        if (barStyleString != null) {
-            templateBuilder.barStyle(Enums.getIfPresent(BarStyle.class, barStyleString.toUpperCase()).or(BarStyle.SOLID));
+        String barColorString = templateFile.getString(DEDConstants.TEMPLATE_BAR_COLOR, "SOLID");
+        if (barColorString != null) {
+            templateBuilder.barStyle(Enums.getIfPresent(BarStyle.class, barColorString.toUpperCase()).or(BarStyle.SOLID));
         }
 
-        String barColorString = templateFile.getString(DEDConstants.TEMPLATE_BAR_STYLE, "PINK");
-        if (barColorString != null) {
-            templateBuilder.barColor(Enums.getIfPresent(BarColor.class, barColorString.toUpperCase()).or(BarColor.PINK));
+        String barStyleString = templateFile.getString(DEDConstants.TEMPLATE_BAR_STYLE, "PINK");
+        if (barStyleString != null) {
+            templateBuilder.barColor(Enums.getIfPresent(BarColor.class, barStyleString.toUpperCase()).or(BarColor.PINK));
         }
 
         templateBuilder.spawnWeight(templateFile.getDouble(DEDConstants.TEMPLATE_SPAWN_WEIGHT, 1));
